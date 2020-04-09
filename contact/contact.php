d55f82b9cb2c30b945b400b1321b05f82cd6994a
deconile <deconile@users.noreply.github.com>
2020-04-09 16:04:40 -0700

    New Site upload

---
+++ contact/contact.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="your@email.address";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
