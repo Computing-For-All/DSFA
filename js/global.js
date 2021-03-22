3259bb099b5c13d9f1bff7ac99d029caf07f8c8d
deconile <deconile@users.noreply.github.com>
2021-03-22 14:44:22 -0700

    allow local page navigation

--- js/global.js
+++ js/global.js
@@ -4,20 +4,20 @@ $(document).ready(
         let pageUrl = document.URL.split('/')[3];
         console.log(pageUrl);
         if(pageUrl === 'DSFA'){
-            pageUrl = '/DSFA'
+            pageUrl = '/DSFA/'
         } else {
-            pageUrl = '';
+            pageUrl = './';
         }
         
         //  NAV LINKS
         let apndNavItems = $('.navbar-items').find('a').first();
         
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'/profiles').html('Students');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'/projects').html('Projects');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'/events').html('Events');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'/visits').html('Visits');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'/about').html('About');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'/contact').html('<i class=\"fas fa-envelope\"></i>');
+        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'profiles').html('Students');
+        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'projects').html('Projects');
+        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'events').html('Events');
+        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'visits').html('Visits');
+        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'about').html('About');
+        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl+'contact').html('<i class=\"fas fa-envelope\"></i>');
     
         $('.navbar-items').find('a').first().remove();
         // END --  NAV LINKS
