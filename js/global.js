c7b7e2e3bc45f9a286cf280e952b2472883645ed
Nathan Akers <ncakers@gmail.com>
2022-09-07 17:03:49 -0700

    improve js navigation html insertion

--- js/global.js
+++ js/global.js
@@ -1,6 +1,7 @@
 $(document).ready(
     function () {
 
+        // NOT SURE WHY THIS EXISTS? LEGACY?
         let pageUrl = document.URL.split('/')[3];
         if (pageUrl === 'DSFA') {
             pageUrl = '/DSFA/'
@@ -8,20 +9,49 @@ $(document).ready(
             pageUrl = '/';
         }
 
-        //  NAV LINKS
-        let apndNavItems = $('.navbar-items').find('a').first();
+        // INSERT NAV HTML
+        let body = document.querySelector('body'),
+            nav = document.createElement('nav');
 
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'profiles').html('Students');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'projects').html('Projects');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'events').html('Events');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'visits').html('Visits');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'about').html('About');
-        apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'contact').html('<i class=\"fas fa-envelope\"></i>');
+        nav.setAttribute('class', 'navbar');
+        nav.innerHTML = `
+            <div class="navbar-logo">
+                <a href="http://computingforall.org" class="cfa-logo"></a>
+                <a href="http://dsfastudents.org/">
+                    <img src="images/logo-color.png" />
+                </a>
+            </div>
+            <div>
+                <div class="navbar-items">
+                    <div class="hamburger">
+                        <i class="fas fa-bars"></i>
+                    </div>
+                    <div class="navbar-list">
+                        <a href="` + pageUrl + `profiles">Students</a>
+                        <a href="` + pageUrl + `projects">Projects</a>
+                        <a href="` + pageUrl + `events">Events</a>
+                        <a href="` + pageUrl + `visits">Visits</a>
+                        <a href="` + pageUrl + `about">About</a>
+                        <a href="` + pageUrl + `contact"><i class=\"fas fa-envelope\"></i></a>
+                    </div>
+                </div>
+            </div>
+        `;
 
-        $('.navbar-items').find('a').first().remove();
-        // END --  NAV LINKS
+        body.prepend(nav);
 
 
+        // let apndNavItems = $('.navbar-items').find('a').first();
+
+        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'profiles').html('Students');
+        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'projects').html('Projects');
+        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'events').html('Events');
+        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'visits').html('Visits');
+        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'about').html('About');
+        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'contact').html('<i class=\"fas fa-envelope\"></i>');
+
+        // $('.navbar-items').find('a').first().remove();
+        // // END --  NAV LINKS
 
     }
 );
