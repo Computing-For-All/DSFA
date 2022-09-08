b1b24e8d0226ef6428dbaac6154314fc42ff3c66
Nathan Akers <ncakers@gmail.com>
2022-09-08 09:46:53 -0700

    insert function has more clarity on what's happening

--- js/global.js
+++ js/global.js
@@ -10,48 +10,32 @@ $(document).ready(
         }
 
         // INSERT NAV HTML
-        let body = document.querySelector('body'),
-            nav = document.createElement('nav');
+        let headerElem = document.querySelector('#js-populate-header');
 
-        nav.setAttribute('class', 'navbar');
-        nav.innerHTML = `
-            <div class="navbar-logo">
-                <a href="http://computingforall.org" class="cfa-logo"></a>
-                <a href="http://dsfastudents.org/">
-                    <img src="images/logo-color.png" />
-                </a>
-            </div>
-            <div>
-                <div class="navbar-items">
-                    <div class="hamburger">
-                        <i class="fas fa-bars"></i>
-                    </div>
-                    <div class="navbar-list">
-                        <a href="` + pageUrl + `profiles">Students</a>
-                        <a href="` + pageUrl + `projects">Projects</a>
-                        <a href="` + pageUrl + `events">Events</a>
-                        <a href="` + pageUrl + `visits">Visits</a>
-                        <a href="` + pageUrl + `about">About</a>
-                        <a href="` + pageUrl + `contact"><i class=\"fas fa-envelope\"></i></a>
+        headerElem.innerHTML = `
+            <nav class="navbar">
+                <div class="navbar-logo">
+                    <a href="http://computingforall.org" class="cfa-logo"></a>
+                    <a href="http://dsfastudents.org/">
+                        <img src="images/logo-color.png">
+                    </a>
+                </div>
+                <div>
+                    <div class="navbar-items">
+                        <div class="hamburger">
+                            <i class="fas fa-bars"></i>
+                        </div>
+                        <div class="navbar-list">
+                            <a href="` + pageUrl + `profiles">Students</a>
+                            <a href="` + pageUrl + `projects">Projects</a>
+                            <a href="` + pageUrl + `events">Events</a>
+                            <a href="` + pageUrl + `visits">Visits</a>
+                            <a href="` + pageUrl + `about">About</a>
+                            <a href="` + pageUrl + `contact"><i class=\"fas fa-envelope\"></i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </nav>
         `;
-
-        body.prepend(nav);
-
-
-        // let apndNavItems = $('.navbar-items').find('a').first();
-
-        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'profiles').html('Students');
-        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'projects').html('Projects');
-        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'events').html('Events');
-        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'visits').html('Visits');
-        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'about').html('About');
-        // apndNavItems.clone().appendTo('.navbar-list').attr('href', pageUrl + 'contact').html('<i class=\"fas fa-envelope\"></i>');
-
-        // $('.navbar-items').find('a').first().remove();
-        // // END --  NAV LINKS
-
     }
 );
