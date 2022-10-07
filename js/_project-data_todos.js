c3b6be49d6d5d36f6235082752b9ade2fd25e966
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-10-07 15:20:16 -0700

    Adding a project-data js file with todos

---
+++ js/_project-data_todos.js
@@ -0,0 +1,19 @@
+
+/* Fill in your project info per the TODOs */
+let gameData = [
+  {
+    // TODO write your project title into the quotes
+    title: "Test Project title",
+    // TODO write a project description into the quotes
+    description: "Test project description",
+    // TODO write the languages used to create the project, enclosed in quotes
+    language: "HTML, CSS",
+    // TODO write you and your teammates' names in the format ["my name","teammate A name","teammate B name"]
+    names: ["testymctesterson","teststudent"],
+    // TODO add the deployed URL which leads to the hosted version of your final project
+    url: "www.google.com", 
+    // TODO write your project thumbnail file name into the quotes
+    imageName: "testproject",
+    filter: "web",
+  },
+]
